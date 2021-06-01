d4f02d78109cf8777aca0f10325248a125eb7632
Ron Moerman <ron@crazyelectron.io>
2021-06-01 19:34:25 +0200

    Docker deploy stage

--- components/Acties.vue
+++ components/Acties.vue
@@ -1,16 +1,16 @@
 <template>
-  <section class="bg-white-800 py-1" id="actie">
-    <div class="container max-w-5xl mx-auto m-8">
-      <h1 class="w-full my-1 text-3xl md:text-3xl lg:text-5xl font-bold leading-tight text-center text-gray">
+  <section class="py-1 bg-white-800" id="actie">
+    <div class="container max-w-5xl m-8 mx-auto">
+      <h1 class="w-full my-1 text-3xl font-bold leading-tight text-center md:text-3xl lg:text-5xl text-gray">
         Introductie actie
       </h1>
       <div class="w-full mb-4">
-        <div class="h-1 mx-auto gradient w-64 opacity-100 my-0 py-0 rounded-t bg-blue"></div>
+        <div class="w-64 h-1 py-0 mx-auto my-0 rounded-t opacity-100 gradient bg-blue"></div>
       </div>
       <actie-left
         headline="Gereduceerd tarief"
         background="bg-white-800"
-        content="Als tijdelijk introductietarief bied ik U een uitgebreide en volledige behandeling aan van uw voeten, hoeveel eelt, kalknagels, likdoorns of nagelriemen ook behandeld moeten worden, <strong>voor de prijs van € 22,95</strong>.<br>Ik neem hierbij alle tijd voor uw voeten en ga niet weg voordat ze weer als van een pasgeboren baby zijn. Zo kunt U het vrijblijvend ervaren."
+        content="Als tijdelijk introductietarief bied ik U een uitgebreide basisbehandeling aan van uw voeten, hoeveel eelt, kalknagels, likdoorns of nagelriemen ook behandeld moeten worden, <strong>voor de prijs van € 22,95</strong>.<br>Ik neem hierbij alle tijd voor uw voeten en ga niet weg voordat ze weer als van een pasgeboren baby zijn. Zo kunt U het vrijblijvend ervaren."
       >
         <template v-slot:image>
           <img src="~assets/img/actietarief.png" alt="actietarief">
